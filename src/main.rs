782985ac53b309c9b1ae9891bcb5fe33ee7ac38a
Nabos <nabos@glargh.fr>
2022-12-12 16:02:06 +0100

    Move after build

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ fn all(config: &State<Config>) -> Result<String, AppError> {
         .current_dir(&config.site_directory)
         .output()?;
 
-    fs::copy(
+    fs::rename(
         Path::new(&config.site_directory).join("/public"),
         &config.output_directory,
     )?;
